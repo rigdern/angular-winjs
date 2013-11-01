f01bfd831baac9f50d7e9e9d0ab0df74c975efcb
josh williams <spankyj@gmail.com>
2013-11-01 14:04:10 -0700

    Changed to have ratings controls in ListView and show that they update a total in the UI when you change them.

--- test/AngularWinJSTest/js/default.js
+++ test/AngularWinJSTest/js/default.js
@@ -4,13 +4,20 @@
     var app = angular.module("app", ["winjs"]);
 
     app.controller("AppController", function ($scope) {
+        var names = ["Josh", "Paul", "Chris", "Ales"]
         $scope.name = "Josh Williams";
         $scope.rating = 3;
         var ratings = [];
-        for (var i = 0; i < 500; i++) {
-            ratings.push(i % 5);
+        for (var i = 0; i < 250; i++) {
+            ratings.push({
+                name: names[i % names.length] + ((i / 4) | 0),
+                rating: (i % 5) + 1
+            });
         }
         $scope.ratings = ratings;
+        $scope.sum = function () {
+            return ratings.reduce(function (t, v) { return t + (+v.rating); }, 0);
+        };
     })
 
 })();
